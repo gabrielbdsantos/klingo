9c1168c18195954f248f74e0d7d3ea93ab090798
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2021-11-02 21:14:18 -0300

    fix(Blade): replace a typo in the constructor

--- klingo/blade.py
+++ klingo/blade.py
@@ -1,11 +1,11 @@
 """Provide tools for managing blades."""
 
-from typing import Sequence
 from profile import Profile
+from typing import Sequence
 
 
 class Blade:
     """Define a simple, generic blade object."""
 
-    def __init__(self, sections = Sequence[Profile]) -> None:
+    def __init__(self, sections: Sequence[Profile]) -> None:
         self.sections = sections
