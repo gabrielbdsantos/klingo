9464d8dec63b5fb728eceb9a3fcf72b585c66b08
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2021-11-23 14:02:05 -0300

    feat: make the Blade class subscriptable

--- klingo/blade.py
+++ klingo/blade.py
@@ -7,5 +7,10 @@ from typing import Sequence
 class Blade:
     """Define a simple, generic blade object."""
 
+    slots = ("sections",)
+
     def __init__(self, sections: Sequence[Profile]) -> None:
         self.sections = sections
+
+    def __getitem__(self, index: int) -> Profile:
+        return self.sections[index]
