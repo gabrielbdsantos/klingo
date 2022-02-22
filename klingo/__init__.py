8a3b30a96c848f3c1fd4b9d3b99e0a52e65bb1ee
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2022-02-22 16:38:47 -0300

    build: update requirements

--- klingo/__init__.py
+++ klingo/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """A minimal framework for creating parameterized airfoil geometries."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .blade import Blade
 from .profile import NACA4, GenericAirfoil
